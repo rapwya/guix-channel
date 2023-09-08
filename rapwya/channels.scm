7daea9edae2783efe0cfdc0b07782b9d8a45e6c0
Wyatt Raphael <wyatt.s.raphael@gmail.com>
2023-09-08 11:58:05 -0700

    Add the make-channel-introduction

--- rapwya/channels.scm
+++ rapwya/channels.scm
@@ -5,4 +5,9 @@
   (channel
     (name 'rapwya)
     (url "https://github.com/warthael/guix-channel")
-    (branch "main")))
+    (branch "main")
+    (introduction 
+      (make-channel-introduction 
+        "9b0c6cc74802490237f8d9a3c924201a6372ed07"
+        (opengpg-fingerprint
+          "363A 8281 E81D 6B7D 55B8  3145 EEEE 86B8 38F0 612E")))))
