205c4cc439ef0f874a71b95176c6a48ee894c6be
Wyatt Raphael <wyatt.s.raphael@gmail.com>
2023-09-08 17:30:27 -0700

    Change commit in make channel intro

--- rapwya/channels.scm
+++ rapwya/channels.scm
@@ -8,6 +8,6 @@
     (branch "main")
     (introduction 
       (make-channel-introduction 
-        "9b0c6cc74802490237f8d9a3c924201a6372ed07"
+        "7daea9edae2783efe0cfdc0b07782b9d8a45e6c0"
         (openpgp-fingerprint
           "363A 8281 E81D 6B7D 55B8  3145 EEEE 86B8 38F0 612E")))))
