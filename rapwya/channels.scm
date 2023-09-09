7364e1d69cbb6653c0e489f18349349d60cc63e8
Wyatt Raphael <wyatt.s.raphael@gmail.com>
2023-09-08 17:04:09 -0700

    Fix spelling mistake

--- rapwya/channels.scm
+++ rapwya/channels.scm
@@ -9,5 +9,5 @@
     (introduction 
       (make-channel-introduction 
         "9b0c6cc74802490237f8d9a3c924201a6372ed07"
-        (opengpg-fingerprint
+        (openpgp-fingerprint
           "363A 8281 E81D 6B7D 55B8  3145 EEEE 86B8 38F0 612E")))))
